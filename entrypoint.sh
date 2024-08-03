d8b7088ff3009d58af97e20bab6512614d4bc1a5
Šimon Lukašík <isimluk@fedoraproject.org>
2024-08-03 14:04:56 +0200

    allow the script to run multiple clients in parallel

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,8 +26,16 @@ s3cmd -d -v la --stats s3://${BUCKET}
 test_file=/tmp/testfile.txt
 dd if=/dev/urandom of=${test_file} bs=1024 count=${FILE_SIZE_KB:-8192}
 
-while true; do
-    remote_filename=$(LC_ALL=C tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 4; echo)
-    s3cmd put --stats ${test_file} s3://${BUCKET}/deletable-perf-test/${nonse}/${remote_filename}
-    s3cmd rm --stats --force s3://${BUCKET}/deletable-perf-test/${nonse}/${remote_filename}
+test() {
+    while true; do
+        remote_filename=$(LC_ALL=C tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 4; echo)
+        s3cmd put --stats ${test_file} s3://${BUCKET}/deletable-perf-test/${nonse}/${remote_filename}
+        s3cmd rm --stats --force s3://${BUCKET}/deletable-perf-test/${nonse}/${remote_filename}
+    done
+}
+
+for i in $(seq "${PROCESSES:-1}"); do
+     test &
 done
+
+wait
